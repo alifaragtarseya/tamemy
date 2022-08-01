9742f3470000b5e9e0e03cf383c56c59d7e8941e
AhmedRamadanTarseya <ahmed.ramadan@tarseya.com>
2022-08-01 09:11:47 +0200

    edit

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -229,18 +229,10 @@
                 items: 1
             }
             , 600: {
-                items: {
-                    {
-                        (App\ Models\ Helper::isMobile()) ? 1: 3
-                    }
-                }
+                items: {{ (App\ Models\ Helper::isMobile()) ? 1: 3}}
             }
             , 1000: {
-                items: {
-                    {
-                        (App\ Models\ Helper::isMobile()) ? 1: 5
-                    }
-                }
+                items: {{(App\ Models\ Helper::isMobile()) ? 1: 5}}
 
             }
         }
